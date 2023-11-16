a62ba76871cf4057ead84faf0220628a3ae4fc7e
Fabrizio Castillo <99932108+Fabrizio004@users.noreply.github.com>
2023-11-16 12:25:32 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,44 @@
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private int idCliente;
+
+    public Cliente(String nombre, String apellido, int idCliente) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", idCliente=" + idCliente +
+                '}';
+    }
+}
---
+++ CuentaBancaria.java
@@ -0,0 +1,56 @@
+public class CuentaBancaria {
+    private int numeroCuenta;
+    private Cliente titular;
+    private double saldo;
+
+    public CuentaBancaria(int numeroCuenta, Cliente titular, double saldoInicial) {
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double cantidad) {
+        saldo += cantidad;
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" +
+                "numeroCuenta=" + numeroCuenta +
+                ", titular=" + titular +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
