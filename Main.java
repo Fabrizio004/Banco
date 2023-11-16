0894acb42474bd69e5498b6bad20a142607298d9
Fabrizio Castillo <99932108+Fabrizio004@users.noreply.github.com>
2023-11-16 12:45:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Crear un cliente
+        
         Cliente cliente1 = new Cliente("John", "Doe", +1);
 
-        // Crear una cuenta bancaria para el cliente
         CuentaBancaria cuenta1 = new CuentaBancaria(1001, cliente1, 1000.0);
-
-        // Realizar operaciones en la cuenta
+        
         cuenta1.depositar(500.0);
         cuenta1.retirar(200.0);
-// En la clase Main
+
         System.out.println("Nombre del cliente: " + cliente1);
         System.out.println("Saldo actual: " + cuenta1.getSaldo());
 
